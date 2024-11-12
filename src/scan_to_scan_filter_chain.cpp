2c757bafc697681f0f9a7ed77211d6ac314c6ccf
Jon Binney <jon.binney@gmail.com>
2024-11-12 23:04:02 +0000

    Rebase from 'release/jazzy/laser_filters'

--- src/scan_to_scan_filter_chain.cpp
+++ src/scan_to_scan_filter_chain.cpp
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2008, Willow Garage, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of the Willow Garage, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,109 +28,112 @@
  */
 
 
-#include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/laser_scan.hpp>
+#include "scan_to_scan_filter_chain.hpp"
 
-// TF
-#include <tf2_ros/transform_listener.h>
-#include "tf2_ros/message_filter.h"
 
-#include "message_filters/subscriber.h"
-
-#include "filters/filter_chain.hpp"
-
-class ScanToScanFilterChain
+// Constructor
+ScanToScanFilterChain::ScanToScanFilterChain(
+  const rclcpp::NodeOptions & options,
+  const std::string & ns)
+: rclcpp::Node("scan_to_scan_filter_chain", ns, options),
+  tf_(NULL),
+  buffer_(this->get_clock()),
+  tf_filter_(NULL),
+  filter_chain_("sensor_msgs::msg::LaserScan")
 {
-protected:
-  // Our NodeHandle
-  rclcpp::Node::SharedPtr nh_;
+  // Configure filter chain
+  filter_chain_.configure(
+    "",
+    this->get_node_logging_interface(), this->get_node_parameters_interface());
 
-  // Components for tf::MessageFilter
-  std::shared_ptr<tf2_ros::TransformListener> tf_;
-  tf2_ros::Buffer buffer_;
+  rcl_interfaces::msg::ParameterDescriptor read_only_desc;
+  read_only_desc.read_only = true;
 
-  message_filters::Subscriber<sensor_msgs::msg::LaserScan> scan_sub_;
-  std::shared_ptr<tf2_ros::MessageFilter<sensor_msgs::msg::LaserScan>> tf_filter_;
-  double tf_filter_tolerance_;
+  // Declare parameters
+  #ifdef RCLCPP_SUPPORTS_MATCHED_CALLBACKS
+  this->declare_parameter("lazy_subscription", false, read_only_desc);
+  #endif
+  this->declare_parameter("tf_message_filter_target_frame", "", read_only_desc);
+  this->declare_parameter("tf_message_filter_tolerance", 0.03, read_only_desc);
 
-  // Filter Chain
-  filters::FilterChain<sensor_msgs::msg::LaserScan> filter_chain_;
+  // Get parameters
+  #ifdef RCLCPP_SUPPORTS_MATCHED_CALLBACKS
+  this->get_parameter("lazy_subscription", lazy_subscription_);
+  #endif
+  this->get_parameter("tf_message_filter_target_frame", tf_message_filter_target_frame_);
+  this->get_parameter("tf_message_filter_tolerance", tf_filter_tolerance_);
 
-  // Components for publishing
-  sensor_msgs::msg::LaserScan msg_;
-  rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr output_pub_;
+  if (!tf_message_filter_target_frame_.empty()) {
+    tf_.reset(new tf2_ros::TransformListener(buffer_));
+    tf_filter_.reset(
+      new tf2_ros::MessageFilter<sensor_msgs::msg::LaserScan>(
+        scan_sub_, buffer_, "",
+        50, this->get_node_logging_interface(), this->get_node_clock_interface()));
+    tf_filter_->setTargetFrame(tf_message_filter_target_frame_);
+    tf_filter_->setTolerance(std::chrono::duration<double>(tf_filter_tolerance_));
 
-public:
-  // Constructor
-  ScanToScanFilterChain(rclcpp::Node::SharedPtr node)
-      : nh_(node),
-        tf_(NULL),
-        buffer_(nh_->get_clock()),
-        scan_sub_(nh_, "scan", rmw_qos_profile_sensor_data),
-        tf_filter_(NULL),
-        filter_chain_("sensor_msgs::msg::LaserScan")
-  {
-    // Configure filter chain
-    filter_chain_.configure("", nh_->get_node_logging_interface(), nh_->get_node_parameters_interface());
-
-    std::string tf_message_filter_target_frame;
-    if (nh_->get_parameter("tf_message_filter_target_frame", tf_message_filter_target_frame))
-    {
-
-      nh_->get_parameter_or("tf_message_filter_tolerance", tf_filter_tolerance_, 0.03);
-
-      tf_.reset(new tf2_ros::TransformListener(buffer_));
-      tf_filter_.reset(new tf2_ros::MessageFilter<sensor_msgs::msg::LaserScan>(scan_sub_, buffer_, "", 50, nh_));
-      tf_filter_->setTargetFrame(tf_message_filter_target_frame);
-      tf_filter_->setTolerance(std::chrono::duration<double>(tf_filter_tolerance_));
-
-      // Setup tf::MessageFilter generates callback
-      tf_filter_->registerCallback(std::bind(&ScanToScanFilterChain::callback, this, std::placeholders::_1));
-    }
-    else 
-    {
-      // Pass through if no tf_message_filter_target_frame
-      scan_sub_.registerCallback(std::bind(&ScanToScanFilterChain::callback, this, std::placeholders::_1));
-    }
-    
-    // Advertise output
-    output_pub_ = nh_->create_publisher<sensor_msgs::msg::LaserScan>("scan_filtered", 1000);
+    // Setup tf::MessageFilter generates callback
+    tf_filter_->registerCallback(
+      std::bind(
+        &ScanToScanFilterChain::callback, this,
+        std::placeholders::_1));
+  } else {
+    // Pass through if no tf_message_filter_target_frame
+    scan_sub_.registerCallback(
+      std::bind(
+        &ScanToScanFilterChain::callback, this,
+        std::placeholders::_1));
   }
 
-  // Destructor
-  ~ScanToScanFilterChain()
-  {
-    if (tf_filter_)
-      tf_filter_.reset();
-    if (tf_)
-      tf_.reset();
+  #ifdef RCLCPP_SUPPORTS_MATCHED_CALLBACKS
+  if (lazy_subscription_) {
+    rclcpp::PublisherOptions pub_options;
+    pub_options.event_callbacks.matched_callback =
+      [this](rclcpp::MatchedInfo & s)
+      {
+        if (s.current_count == 0) {
+          scan_sub_.unsubscribe();
+        } else if (!scan_sub_.getSubscriber()) {
+          scan_sub_.subscribe(this, "scan", rmw_qos_profile_sensor_data);
+        }
+      };
+    output_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>(
+      "scan_filtered", 1000, pub_options);
+  } else {
+    output_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>("scan_filtered", 1000);
+    scan_sub_.subscribe(this, "scan", rmw_qos_profile_sensor_data);
   }
-
-  // Callback
-  void callback(const std::shared_ptr<const sensor_msgs::msg::LaserScan>& msg_in)
-  {
-    // Run the filter chain
-    if (filter_chain_.update(*msg_in, msg_))
-    {
-      //only publish result if filter succeeded
-      output_pub_->publish(msg_);
-    }
-  }
-};
-
-int main(int argc, char **argv)
-{
-  rclcpp::init(argc, argv);
-  auto nh = rclcpp::Node::make_shared("scan_to_scan_filter_chain");
-  ScanToScanFilterChain t(nh);
-
-  rclcpp::WallRate loop_rate(200);
-  while (rclcpp::ok()) {
-
-    rclcpp::spin_some(nh);
-    loop_rate.sleep();
-
-  }
-
-  return 0;
+  #else
+  output_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>("scan_filtered", 1000);
+  scan_sub_.subscribe(this, "scan", rmw_qos_profile_sensor_data);
+  #endif
 }
+
+// Destructor
+ScanToScanFilterChain::~ScanToScanFilterChain()
+{
+  if (tf_filter_) {
+    tf_filter_.reset();
+  }
+  if (tf_) {
+    tf_.reset();
+  }
+}
+
+// Callback
+void ScanToScanFilterChain::callback(
+  const std::shared_ptr<const sensor_msgs::msg::LaserScan> & msg_in)
+{
+  // Run the filter chain
+  if (filter_chain_.update(*msg_in, msg_)) {
+    //only publish result if filter succeeded
+    output_pub_->publish(msg_);
+  }
+}
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(ScanToScanFilterChain)
